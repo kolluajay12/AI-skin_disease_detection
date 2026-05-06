bf83fab3b38c5b855534bc8a270a336e843aad0c
KOLLU AJAY <120007593+kolluajay12@users.noreply.github.com>
2026-05-06 14:24:32 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ login }) => {
     setError('');
     setLoading(true);
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, formData);
+      const res = await axios.post(`${https://ai-skin-disease-detection-9.onrender.com}/api/auth/login`, formData);
       login(res.data.user, res.data.token);
       navigate('/dashboard');
     } catch (err) {
