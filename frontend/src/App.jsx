77e75a658055c890cdc69e87b4f6f9254eceabe4
KOLLU AJAY <120007593+kolluajay12@users.noreply.github.com>
2026-05-06 14:26:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      axios.get(`${import.meta.env.VITE_API_URL}/api/auth/user`, {
+      axios.get(`${https://ai-skin-disease-detection-9.onrender.com}/api/auth/user`, {
         headers: { 'x-auth-token': token }
       }).then(res => {
         setUser(res.data);
